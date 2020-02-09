a132aa9b6ef2dcb4e2fadd19252123a02abba9a2
nasaku898 <simonlim898@hotmail.com>
2020-02-09 01:00:06 -0500

    feature/#10 refactor code for registration service

--- backend/src/main/java/com/soen341/instagram/service/impl/RegistrationService.java
+++ backend/src/main/java/com/soen341/instagram/service/impl/RegistrationService.java
@@ -25,52 +25,52 @@ public class RegistrationService
 
 	public void createNewAccount(final Account account)
 	{
-		if (!isEmailTaken(account.getEmail()) && !isUsernameTaken(account.getUsername()))
-		{
-			if (!isEmailFormatValid(account.getEmail()))
-			{
-				throw new InvalidEmailFormatException();
-			}
+		checkIfEmailTaken(account.getEmail());
 
-			if (!isUsernameFormatValid(account.getUsername()))
-			{
-				throw new InvalidUsernameFormatException();
-			}
+		checkIfUsernameTaken(account.getUsername());
 
-			account.setPassword(passwordEncoder.encode(account.getPassword()));
-			account.setCreated(new Date());
-			accountRepository.save(account);
-		}
+		checkIfEmailFormatValid(account.getEmail());
+
+		checkIfUsernameFormatValid(account.getUsername());
+
+		account.setPassword(passwordEncoder.encode(account.getPassword()));
+		account.setCreated(new Date());
+		accountRepository.save(account);
 	}
 
-	protected boolean isEmailTaken(final String email)
+	private void checkIfEmailTaken(final String email)
 	{
 		if (!(accountRepository.findByEmail(email) == null))
 		{
 			throw new EmailTakenException();
 		}
-		return false;
 	}
 
-	protected boolean isUsernameTaken(final String username)
+	private void checkIfUsernameTaken(final String username)
 	{
 		if (!(accountRepository.findByUsername(username) == null))
 		{
 			throw new UsernameTakenException();
 		}
-		return false;
 	}
 
-	protected boolean isEmailFormatValid(final String email)
+	private void checkIfEmailFormatValid(final String email)
 	{
 		String regex = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
-		return Pattern.matches(regex, email);
+		if (!Pattern.matches(regex, email))
+		{
+			throw new InvalidEmailFormatException();
+		}
 	}
 
-	protected boolean isUsernameFormatValid(final String username)
+	private void checkIfUsernameFormatValid(final String username)
 	{
 		String regex = "[\\w+.*\\d+]+";
-		return Pattern.matches(regex, username);
+		if (!Pattern.matches(regex, username))
+		{
+			throw new InvalidUsernameFormatException();
+		}
+
 	}
 
 }
