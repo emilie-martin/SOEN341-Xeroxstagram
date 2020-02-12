ffe29047001b14e315edb207d2a4befd80daa111
Luc Nguyen <1999lucnguyen@gmail.com>
2020-02-12 11:46:47 -0500

    #30 Implement LoginController

--- backend/src/main/java/com/soen341/instagram/controller/LoginController.java
+++ backend/src/main/java/com/soen341/instagram/controller/LoginController.java
@@ -1,9 +1,31 @@
 package com.soen341.instagram.controller;
 
+import com.soen341.instagram.dto.account.LoginRequestDTO;
+import com.soen341.instagram.dto.account.RefreshLoginRequestDTO;
+import com.soen341.instagram.service.impl.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import javax.validation.Valid;
 
 @RestController
 public class LoginController
 {
 
+    @Autowired
+    private LoginService loginService;
+
+    @PostMapping(value = "account/login")
+    public ResponseEntity<String> login(@RequestBody @Valid LoginRequestDTO loginRequestDTO)
+    {
+        return loginService.login(loginRequestDTO.getUsername(), loginRequestDTO.getPassword());
+    }
+
+    @PostMapping(value = "account/refresh")
+    public ResponseEntity<String> refreshToken(@RequestBody @Valid RefreshLoginRequestDTO refreshLoginRequestDTO)
+    {
+        return loginService.refreshToken(refreshLoginRequestDTO.getToken());
+    }
 }
