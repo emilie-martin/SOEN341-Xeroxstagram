72fc9593200709aa7395ae53aec5a3b8d2bed9d1
emilie-martin <emmy.mar@hotmail>
2020-02-25 15:11:57 -0500

    #22 Implement account service

---
+++ backend/src/main/java/com/soen341/instagram/service/impl/AccountService.java
@@ -0,0 +1,64 @@
+package com.soen341.instagram.service.impl;
+
+import java.util.Date;
+import java.util.regex.Pattern;
+
+// Spring
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import com.soen341.instagram.exception.account.InvalidEmailFormatException;
+import com.soen341.instagram.security.userDetail;
+import com.soen341.instagram.security.userDetailsService;
+
+// Project
+import com.soen341.instagram.dao.impl.AccountRepository;
+import com.soen341.instagram.model.Account;
+
+@Service
+public class AccountService 
+{
+	@Autowired
+	AccountRepository accountRepository;
+
+  private void updatePassword(String newPassword) {
+    if(!getCurrentAccount().getPassword().equals(newPassword)) {
+      throw ("The new password cannot be the same as the old password");
+    }
+    else {
+      getCurrentAccount().setPassword(newPassword);
+    }
+  }
+
+  private String getEmail() {
+    return getCurrentAccount().getEmail();
+  }
+
+  private void updateEmail(String newEmail) {
+		String regex = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+
+		if (!Pattern.matches(regex, email))
+		{
+			throw new InvalidEmailFormatException();
+		}
+    else {
+      getCurrentAccount().setEmail(newEmail);
+    }
+  }
+
+  private Date getBirthday() {
+    return getCurrentAccount().getBirthday();
+  }
+
+  private void updateBirthday(Date bday) {
+    return getCurrentAccount().setDateOfBirth(bday);
+  }
+
+  // Helper method
+  public Account getCurrentAccount() {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+    String username = ((UserDetails)authentication.getPrincipal()).getUsername();
+
+    return accountRepository.findByUsername(username);
+  }
+}
