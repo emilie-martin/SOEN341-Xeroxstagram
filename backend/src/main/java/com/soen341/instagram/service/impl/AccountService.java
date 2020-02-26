2e540999f054302aade6e9a734ef6f84d4e9fd78
emilie-martin <emmy.mar@hotmail>
2020-02-26 15:56:05 -0500

    #22 Save modified data to account repository in account service

--- backend/src/main/java/com/soen341/instagram/service/impl/AccountService.java
+++ backend/src/main/java/com/soen341/instagram/service/impl/AccountService.java
@@ -20,12 +20,14 @@ public class AccountService
 	@Autowired
 	AccountRepository accountRepository;
 
-  private void updatePassword(String newPassword) {
+  private void updatePassword(final String newPassword) {
     if(!getCurrentAccount().getPassword().equals(newPassword)) {
       throw ("The new password cannot be the same as the old password");
     }
     else {
-      getCurrentAccount().setPassword(newPassword);
+      final Account account = getCurrentAccount();
+      account.setPassword(newPassword);
+      accountRepository.save(account);
     }
   }
 
@@ -33,7 +35,7 @@ public class AccountService
     return getCurrentAccount().getEmail();
   }
 
-  private void updateEmail(String newEmail) {
+  private void updateEmail(final String newEmail) {
 		String regex = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
 
 		if (!Pattern.matches(regex, email))
@@ -41,7 +43,9 @@ public class AccountService
 			throw new InvalidEmailFormatException();
 		}
     else {
-      getCurrentAccount().setEmail(newEmail);
+      final Account account = getCurrentAccount();
+      account.setEmail(newEmail);
+      accountRepository.save(account);
     }
   }
 
@@ -49,8 +53,10 @@ public class AccountService
     return getCurrentAccount().getBirthday();
   }
 
-  private void updateBirthday(Date bday) {
-    return getCurrentAccount().setDateOfBirth(bday);
+  private void updateBirthday(final Date bday) {
+    final Account account = getCurrentAccount();
+    account.setDateOfBirth(bday);
+    accountRepository.save(account);
   }
 
   // Helper method
