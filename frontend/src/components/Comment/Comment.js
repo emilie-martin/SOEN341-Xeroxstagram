34a2aedcc2b3945769afa720abf6a023a57af0c3
nasaku898 <simonlim898@hotmail.com>
2020-02-28 17:19:51 -0500

    #40 Refactored code

--- frontend/src/components/Comment/Comment.js
+++ frontend/src/components/Comment/Comment.js
@@ -1,14 +1,14 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from "react-router-dom"
 import './SCSS/Comment.scss'
 import { timeElapseSincePosted } from '../../services/TimeService'
 export const Comment = (props) => {
     const [timePosted, setTimePosted] = useState("");
-    
+
     useEffect(() => {
-       setTimePosted(timeElapseSincePosted(new Date(props.comment.created)));
+        setTimePosted(timeElapseSincePosted(new Date(props.comment.created)));
     }, []);
-    
+
     return (
         <div className="comment-div">
             <br />
--- frontend/src/services/TimeService.js
+++ frontend/src/services/TimeService.js
@@ -4,28 +4,28 @@ export const timeElapseSincePosted = (date) => {
     let timePosted = currentDate.getTime() - date.getTime();
     timePosted = (timePosted / 1000).toFixed(0);
 
-    if(timePosted < 60){
+    if (timePosted < 60) {
         timePosted = (timePosted).toFixed(0);
     }
-    else if(timePosted < 3600){
+    else if (timePosted < 3600) {
         //Display in minutes
-        timePosted = (timePosted/60).toFixed(0);
+        timePosted = (timePosted / 60).toFixed(0);
         timeFormat = "min";
     }
-    else if(timePosted <86400){
+    else if (timePosted < 86400) {
         //Display in hours
-        timePosted = (timePosted/(60*60)).toFixed(0);
+        timePosted = (timePosted / (60 * 60)).toFixed(0);
         timeFormat = "h";
     }
-    else if(timePosted < 1036800){
+    else if (timePosted < 1036800) {
         //Display in days
-        timePosted = (timePosted/(60*60*24)).toFixed(0);
-        timeFormat ="days";
+        timePosted = (timePosted / (60 * 60 * 24)).toFixed(0);
+        timeFormat = "days";
     }
-    else if(timePosted >= 1036800){
-        timePosted = (timePosted/(60*60*24*12)).toFixed(0);
+    else if (timePosted >= 1036800) {
+        timePosted = (timePosted / (60 * 60 * 24 * 12)).toFixed(0);
         timeFormat = "years";
     }
-           
-    return timePosted + timeFormat;
+
+    return timePosted + " " + timeFormat;
 }
