394126957ab71b0ea86bb30fc851768dc19c8f6f
karin kazarian <karinghazarian@gmail.com>
2020-02-27 20:22:49 -0500

    #28 added unit test for registeration service

--- backend/src/main/java/com/soen341/instagram/dao/impl/PictureRepository.java
+++ backend/src/main/java/com/soen341/instagram/dao/impl/PictureRepository.java
@@ -3,9 +3,11 @@ package com.soen341.instagram.dao.impl;
 import com.soen341.instagram.model.Account;
 import com.soen341.instagram.model.Picture;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PictureRepository extends PagingAndSortingRepository<Picture, Long> {
     List<Picture> findByAccount(Account account);
 }
