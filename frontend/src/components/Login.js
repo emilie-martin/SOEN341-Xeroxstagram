d4945816e8c587a87b949f172253f72deda58af8
Daniela Venuta <44103490+daniela-venuta@users.noreply.github.com>
2020-02-28 14:48:58 -0500

    #38 Add Login.scss and edit functional login component

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -2,6 +2,7 @@ import React from "react";
 import axios from "axios";
 import { useState } from "react"; 
 import '../config'
+import './Login.scss'
 
 export const Login = (props)=> {
 
@@ -30,22 +31,25 @@ export const Login = (props)=> {
                 }
             )
     };
-        return (
-            <div className="login">
-                <form onSubmit={submit}>
+
+    return (
+        <div className="login">
+            <form onSubmit={submit}>
+                <div className="login-username">
                     <label>Username</label>
-                    <input name="username" placeholder="Username"/>
                     <br/>
+                    <input name="username" placeholder="Enter username" className="login-field"/>
+                </div>
+                <div className="login-password">
                     <label>Password</label>
-                    <input name="password" type="password" placeholder="Password"/>
                     <br/>
-                    {errorMsg && <div className="error">Error: {errorMsg}</div>}
-                    <button type="submit">
-                        Login
-                    </button>
-                </form>
-            </div>
-        );
+                    <input name="password" type="password" placeholder="Enter password" className="login-field"/>
+                </div>
+                <button className="button" type="submit">Login</button>
+                {errorMsg && <div className="error">{errorMsg}</div>}
+            </form>
+        </div>
+    );
 }
 
 export default Login;
