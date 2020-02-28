97dbaf117d5a437b8a54051a27e60205f66d1a29
karin kazarian <karinghazarian@gmail.com>
2020-02-27 20:22:49 -0500

    #28 added unit test for registeration service

--- backend/src/main/java/com/soen341/instagram/dao/impl/AccountRepository.java
+++ backend/src/main/java/com/soen341/instagram/dao/impl/AccountRepository.java
@@ -2,7 +2,9 @@ package com.soen341.instagram.dao.impl;
 
 import com.soen341.instagram.model.Account;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AccountRepository extends PagingAndSortingRepository<Account, String> {
     Account findByUsername(String username);
     Account findByEmail(String email);
