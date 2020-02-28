7273166bb08c7ef681ad95c630f6ac379df0c71b
Daniela Venuta <44103490+daniela-venuta@users.noreply.github.com>
2020-02-28 11:23:35 -0500

    #38 Convert Login to functional component and add stylesheet file

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,16 +1,13 @@
 import React from "react";
 import axios from "axios";
+import { useState } from "react"; 
 import '../config'
 
-class Login extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            errorMsg: ""
-        }
-    }
+export const Login = (props)=> {
 
-    submit(event) {
+    const [errorMsg, setErrorMsg] = useState("");
+
+    const  submit = (event)=>{
         event.preventDefault();
         axios.post(global.config.BACKEND_URL + "/account/login",
             {
@@ -19,36 +16,36 @@ class Login extends React.Component {
             })
             .then(
                 (response) => {
-                    this.props.onSuccess(response);
+                    props.onSuccess(response);
                 },
                 (error) => {
-                    if (error.response) {
-                        this.setState({errorMsg: "Invalid credentials"});
-                    } else {
-                        this.setState({errorMsg: "An unknown error occurred."});
+                    if (error.response)
+                    {
+                        setErrorMsg("Invalid credentials");
+                    } 
+                    else
+                    {
+                        setErrorMsg("An unknown error occurred");
                     }
                 }
             )
-    }
-
-    render() {
+    };
         return (
             <div className="login">
-                <form onSubmit={this.submit.bind(this)}>
+                <form onSubmit={submit}>
                     <label>Username</label>
-                    <input name="username"/>
+                    <input name="username" placeholder="Username"/>
                     <br/>
                     <label>Password</label>
-                    <input name="password" type="password"/>
+                    <input name="password" type="password" placeholder="Password"/>
                     <br/>
-                    {this.state.errorMsg && <div className="error">Error: {this.state.errorMsg}</div>}
+                    {errorMsg && <div className="error">Error: {errorMsg}</div>}
                     <button type="submit">
                         Login
                     </button>
                 </form>
             </div>
         );
-    }
 }
 
 export default Login;
