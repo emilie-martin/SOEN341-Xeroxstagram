22ff79f5274fbeb8c49ef14135370a692f13dcce
nasaku898 <simonlim898@hotmail.com>
2020-03-04 11:25:29 -0500

    #65 Added comment to clarify UnauthorizedRightsException

--- backend/src/main/java/com/soen341/instagram/exception/comment/UnauthorizedRightsException.java
+++ backend/src/main/java/com/soen341/instagram/exception/comment/UnauthorizedRightsException.java
@@ -3,6 +3,7 @@ package com.soen341.instagram.exception.comment;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+//Throws exception when a user tries to delete a comment that isn't their's
 @ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class UnauthorizedRightsException extends RuntimeException
 {
