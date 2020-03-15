ad76d3d286f38d4122a4cf55eebff97dda4dc101
emilie-martin <emmy.mar@hotmail>
2020-03-15 12:58:35 -0400

    #92 Made Post component conform to coding standards

--- frontend/src/components/Post/Post.js
+++ frontend/src/components/Post/Post.js
@@ -1,46 +1,43 @@
-import React, { useState, useEffect } from 'react'
+import '../../config';
 import axios from "axios";
 import { Link } from "react-router-dom";
-import { PostComment } from "../Comment/PostComment";
-import "./Post.scss";
-import '../../config';
-import CommentList from "../Comment/CommentList";
-const Post = (props) => {
+import React, { useState, useEffect } from 'react'
 
+import CommentList from "../Comment/CommentList";
+import PostComment from "../Comment/PostComment";
+
+import "./Post.scss";
+
+export default function Post(props) {
     const [Picture, setPicture] = useState(undefined);
     const [refreshComment, setRefreshComment] = useState(false);
 
     useEffect(() => {
         const loadPicture = () => {
             axios.get(global.config.BACKEND_URL + "/picture/" + props.id).then(
-                (response) => {
-                    setPicture(response.data);
-                }
-            ).catch(
-                () => {
-                    setPicture(null);
-                }
+                (response) => {setPicture(response.data);}
+            ).catch( () => {setPicture(null);}
             )
         }
+
         loadPicture();
     }, [props.id])
 
     const onCommentPosted = () => {
-        //Whenver a comment is posted, inverse the boolean associated to refreshComment
-        //This state will be passed into a commentList in order to make the component rerender
+        // Whenever a comment is posted, inverse the boolean associated to refreshComment
+        // This state will be passed into a commentList in order to make the component rerender
         setRefreshComment(!refreshComment);
     }
 
     return (
         <div>
-            {Picture ?
-                <div className="post">
+            {Picture
+                ? <div className="post">
                     <div className="image-wrapper">
                         <img src={`${global.config.BACKEND_URL}/picture/${props.id}.jpg`}
                             alt={`${props.id}`} />
                     </div>
                     <div className="text-wrapper">
-                        {/* a Description component can be created to facilitate the creation of Post components */}
                         <div className="post-description">
                             <div className="account-name">
                                 <Link to={`/account/${Picture.account}`}>
@@ -52,15 +49,13 @@ const Post = (props) => {
                         <div className="comments">
                             <CommentList refreshComment={refreshComment} postId={props.id} />
                         </div>
-                        <div className="posts-Comment">
+                        <div className="posts-comment">
                             <PostComment postId={props.id} onCommentPosted={onCommentPosted} />
                         </div>
                     </div>
-                </div>
-                :
-                Picture && <div className="error">The picture could not be found.</div>
+                 </div>
+                : Picture && <div className="error"><p>The picture could not be found.</p></div>
             }
         </div>
     )
 }
-export default Post;
