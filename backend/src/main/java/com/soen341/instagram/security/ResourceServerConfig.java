982defb3715c09ce6b8273eb8bb9af6163da8eae
nasaku898 <simonlim898@hotmail.com>
2020-03-14 23:04:44 -0400

    #83 Modified getProfile implementation to allow annonymous request

--- backend/src/main/java/com/soen341/instagram/security/ResourceServerConfig.java
+++ backend/src/main/java/com/soen341/instagram/security/ResourceServerConfig.java
@@ -19,13 +19,12 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter
 	@Override
 	public void configure(HttpSecurity http) throws Exception
 	{
-		http.cors().and().headers().frameOptions().disable().and().authorizeRequests()
-				.antMatchers("/picture").authenticated()
-				.antMatchers("/comment/commentByPicture/**", "/comment/commentById/**").permitAll()
-				.antMatchers("/comment/**").authenticated()
-				.antMatchers("/account/profile/**").authenticated()
-				.antMatchers("/account/following/**").authenticated();
-		
+		http.cors().and().headers().frameOptions().disable().and().authorizeRequests().antMatchers("/picture")
+				.authenticated().antMatchers("/comment/commentByPicture/**", "/comment/commentById/**").permitAll()
+				.antMatchers("/comment/**").authenticated().antMatchers("/account/profile/{username}").permitAll()
+				.antMatchers("/account/profile/**").authenticated().antMatchers("/account/following/**")
+				.authenticated();
+
 		// I'm letting all the request as permitAll for now. It will be easier to
 		// develop API. We can configure the authentication later
 	}
