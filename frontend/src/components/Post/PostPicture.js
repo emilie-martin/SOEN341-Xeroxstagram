f81acd0be7a25403113ac986b7d2cb527db62183
karin kazarian <karinghazarian@gmail.com>
2020-03-16 22:54:31 -0400

    #42 changed the PostPicture from class to function component

--- frontend/src/components/Post/PostPicture.js
+++ frontend/src/components/Post/PostPicture.js
@@ -1,56 +1,47 @@
 import React from "react";
+import { useState } from "react";
 import axios from "axios";
-import '../../config';
+import "../../config";
 
-class PostPicture extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            errorMsg: ""
+export const PostPicture = props => {
+  const [errorMsg, setErrorMsg] = useState("");
+
+  const submit = event => {
+    event.preventDefault();
+    const data = new FormData();
+    data.append("picture", event.target.picture.files[0]);
+    data.append("caption", event.target.caption.value);
+    axios.post(global.config.BACKEND_URL + "/picture", data).then(
+      response => {
+        this.props.history.push(`/post/${response.data.id}`);
+      },
+      e => {
+        if (e.response) {
+          if (e.response.status === 401) {
+            setErrorMsg("You must be logged in.");
+          } else {
+            setErrorMsg(e.response.data.message);
+          }
+        } else {
+          setErrorMsg("An unknown error occured.");
         }
-    }
-    submit(event) {
-        event.preventDefault();
-        const data = new FormData();
-        data.append('picture', event.target.picture.files[0]);
-        data.append('caption', event.target.caption.value);
-        axios.post(global.config.BACKEND_URL + "/picture", data)
-            .then(
-                (response) => {
-                    this.props.history.push(`/post/${response.data.id}`);
-                },
-                (e) => {
-                    if (e.response) {
-                        if(e.response.status === 401) {
-                            this.setState({errorMsg: "You must be logged in."});
-                        } else {
-                            this.setState({errorMsg: e.response.data.message});
-                        }
-                    } else {
-                        this.setState({errorMsg: "An unknown error occured."});
-                    }
-                }
-            )
-    }
+      }
+    );
+  };
 
-    render() {
-        return (
-            <div className="postPicture">
-                <form onSubmit={this.submit.bind(this)}>
-                    <label>File</label>
-                    <input name="picture" type="file"/>
-                    <br/>
-                    <label>Caption</label>
-                    <input name="caption"/>
-                    <br/>
-                    {this.state.errorMsg && <div className="error">Error: {this.state.errorMsg}</div>}
-                    <button type="submit">
-                        Post
-                    </button>
-                </form>
-            </div>
-        );
-    }
-}
-
+  return (
+    <div className="postPicture">
+      <form onSubmit={submit}>
+        <label>File</label>
+        <input name="picture" type="file" />
+        <br />
+        <label>Caption</label>
+        <input name="caption" />
+        <br />
+        {errorMsg && <div className="error">Error: {errorMsg}</div>}
+        <button type="submit">Post</button>
+      </form>
+    </div>
+  );
+};
 export default PostPicture;
