034e13b337774b91a6cf3eeec3248c44f300a6e1
Luc Nguyen <1999lucnguyen@gmail.com>
2020-03-18 12:33:00 -0400

    #88 Temporarily refactoring test

--- backend/src/test/java/com/soen341/instagram/service/impl/PictureServiceTest.java
+++ backend/src/test/java/com/soen341/instagram/service/impl/PictureServiceTest.java
@@ -7,11 +7,12 @@ import com.soen341.instagram.exception.picture.NotAPictureException;
 import com.soen341.instagram.exception.picture.PictureNotFoundException;
 import com.soen341.instagram.model.Account;
 import com.soen341.instagram.model.Picture;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.modelmapper.ModelMapper;
@@ -23,9 +24,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.mockito.Mockito.doReturn;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(MockitoJUnitRunner.class)
 public class PictureServiceTest
@@ -45,23 +44,23 @@ public class PictureServiceTest
     private Picture pic;
     private PictureDTO picDTO;
 
-    @BeforeEach
-    public void beforeEach()
+    @Before
+    public void setup()
     {
-        MockitoAnnotations.initMocks(this);
+        //MockitoAnnotations.initMocks(this);
 
-        doReturn("test account").when(account).getUsername();
+        Mockito.when(account.getUsername()).thenReturn("test account");
 
         pic = new Picture();
         pic.setId(1);
         pic.setAccount(account);
 
-        doReturn(Optional.of(pic)).when(pictureRepository).findById(1L);
+        Mockito.when(pictureRepository.findById(1L)).thenReturn(Optional.of(pic));
 
         picDTO = new PictureDTO();
         picDTO.setId(1L);
 
-        doReturn(picDTO).when(modelMapper).map(pic, PictureDTO.class);
+        Mockito.when(modelMapper.map(pic, PictureDTO.class)).thenReturn(picDTO);
     }
 
     @Test
@@ -70,54 +69,51 @@ public class PictureServiceTest
         final String caption = "caption";
         final MockMultipartFile file = getTestFile("src/test/resources/1pixel.jpg");
         Picture uploadedPicture = pictureService.uploadPicture(caption, file, account);
-        assertThat(uploadedPicture.getAccount()).isEqualTo(account);
-        assertThat(uploadedPicture.getCaption()).isEqualTo(caption);
+        assertEquals(account, uploadedPicture.getAccount());
+        assertEquals(caption, uploadedPicture.getCaption());
     }
 
-    @Test
+    @Test(expected = NotAPictureException.class)
     public void testUploadPictureNotValidFile()
     {
         final String caption = "caption";
         final MockMultipartFile file = getTestFile("src/test/resources/invalidPicture.txt");
-        assertThatCode(() -> pictureService.uploadPicture(caption, file, account))
-                .isInstanceOf(NotAPictureException.class);
+        pictureService.uploadPicture(caption, file, account);
     }
 
     @Test
     public void testGetPictureDTOFromId()
     {
         PictureDTO pic = pictureService.getPictureDTOFromId("1");
-        assertThat(pic.getAccount()).isEqualTo("test account");
-        assertThat(pic.getId()).isEqualTo(1L);
+        assertEquals("test account", pic.getAccount());
+        assertEquals(1L, pic.getId());
     }
 
-    @Test
+    @Test(expected = PictureNotFoundException.class)
     public void testGetPictureDTOFromId_NotFound()
     {
-        assertThatCode(() -> pictureService.getPictureDTOFromId("2"))
-                .isInstanceOf(PictureNotFoundException.class);
+        pictureService.getPictureDTOFromId("2");
     }
 
-    @Test
+    @Test(expected = InvalidIdException.class)
     public void testGetPictureDTOFromId_InvalidId()
     {
-        assertThatCode(() -> pictureService.getPictureDTOFromId("invalid"))
-                .isInstanceOf(InvalidIdException.class);
+        pictureService.getPictureDTOFromId("invalid");
     }
 
     @Test
     public void testGetAccountPictures()
     {
-        doReturn(Collections.singletonList(pic)).when(pictureRepository).findByAccount(account);
+        Mockito.when(pictureRepository.findByAccount(account)).thenReturn(Collections.singletonList(pic));
         List<Long> pictures = pictureService.getAccountPictures(account);
-        assertThat(pictures.size()).isEqualTo(1);
-        assertThat(pictures.get(0)).isEqualTo(1L);
+        assertEquals(1, pictures.size());
+        assertEquals(1L, (long)pictures.get(0));
     }
 
     @Test
     public void testToPictureDTO()
     {
-        assertThat(pictureService.toPictureDTO(pic)).isEqualTo(picDTO);
+        assertEquals(picDTO, pictureService.toPictureDTO(pic));
     }
 
     private MockMultipartFile getTestFile(String filePath) {
