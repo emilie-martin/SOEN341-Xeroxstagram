d69e02d7f8077c5be18cafea1d6f0876a2fbc8a0
mint47 <dor012299@gmail.com>
2020-03-25 22:37:58 -0400

    fixed indentation in DTO

--- backend/src/main/java/com/soen341/instagram/dto/comment/CommentResponseDTO.java
+++ backend/src/main/java/com/soen341/instagram/dto/comment/CommentResponseDTO.java
@@ -4,8 +4,7 @@ import java.util.Date;
 
 import com.soen341.instagram.dto.picture.PictureDTO;
 
-public class CommentResponseDTO
-{
+public class CommentResponseDTO {
 	private long id;
 	private String comment;
 	private Date created;
@@ -15,83 +14,67 @@ public class CommentResponseDTO
 	private boolean editable;
 	private int likeCount;
 
-	public int getNbLikes()
-	{
+	public int getNbLikes() {
 		return nbLikes;
 	}
 
-	public void setNbLikes(int nbLikes)
-	{
+	public void setNbLikes(int nbLikes) {
 		this.nbLikes = nbLikes;
 	}
 
-	public String getAccount()
-	{
+	public String getAccount() {
 		return account;
 	}
 
-	public void setAccount(String account)
-	{
+	public void setAccount(String account) {
 		this.account = account;
 	}
 
-	public PictureDTO getPictureDTO()
-	{
+	public PictureDTO getPictureDTO() {
 		return pictureDTO;
 	}
 
-	public void setPictureDTO(PictureDTO pictureDTO)
-	{
+	public void setPictureDTO(PictureDTO pictureDTO) {
 		this.pictureDTO = pictureDTO;
 	}
 
-	public long getId()
-	{
+	public long getId() {
 		return id;
 	}
 
-	public void setId(long id)
-	{
+	public void setId(long id) {
 		this.id = id;
 	}
 
-	public String getComment()
-	{
+	public String getComment() {
 		return comment;
 	}
 
-	public void setComment(String comment)
-	{
+	public void setComment(String comment) {
 		this.comment = comment;
 	}
 
-	public Date getCreated()
-	{
+	public Date getCreated() {
 		return created;
 	}
 
-	public void setCreated(Date created)
-	{
+	public void setCreated(Date created) {
 		this.created = created;
 	}
 
-	public boolean isEditable()
-	{
+	public boolean isEditable() {
 		return editable;
 	}
 
-	public void setEditable(boolean editable)
-	{
+	public void setEditable(boolean editable) {
 		this.editable = editable;
 	}
 
-	public int getLikeCount()
-	{
+	public int getLikeCount() {
 		return likeCount;
 	}
-	
-	public void setLikeCount(int likeCount)
-	{
+
+	public void setLikeCount(int likeCount) {
 		this.likeCount = likeCount;
 	}
 
