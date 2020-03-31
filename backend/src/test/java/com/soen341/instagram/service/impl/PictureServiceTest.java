c800b03bc3ac1b666e30e62a73085f8dd89fea8a
Luc Nguyen <1999lucnguyen@gmail.com>
2020-03-30 23:52:22 -0400

    #88 Mock file writing

--- backend/src/test/java/com/soen341/instagram/service/impl/PictureServiceTest.java
+++ backend/src/test/java/com/soen341/instagram/service/impl/PictureServiceTest.java
@@ -7,18 +7,26 @@ import com.soen341.instagram.exception.picture.NotAPictureException;
 import com.soen341.instagram.exception.picture.PictureNotFoundException;
 import com.soen341.instagram.model.Account;
 import com.soen341.instagram.model.Picture;
+import com.soen341.instagram.utils.UserAccessor;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.modelmapper.ModelMapper;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.springframework.mock.web.MockMultipartFile;
 
+import javax.imageio.ImageIO;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -26,6 +34,7 @@ import java.util.Optional;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(PowerMockRunner.class)
+@PrepareForTest(PictureService.class)
 public class PictureServiceTest
 {
     @InjectMocks
@@ -39,6 +48,9 @@ public class PictureServiceTest
 
     @Mock
     private Account account;
+
+    @Rule
+    private TemporaryFolder tempFolder = new TemporaryFolder();
 
     private Picture pic;
     private PictureDTO picDTO;
@@ -65,7 +77,8 @@ public class PictureServiceTest
     {
         final String caption = "caption";
         final MockMultipartFile file = getTestFile("src/test/resources/1pixel.jpg");
-        Picture uploadedPicture = pictureService.uploadPicture(caption, file, account);
+        mockFileCreation();
+        final Picture uploadedPicture = pictureService.uploadPicture(caption, file, account);
         assertEquals(account, uploadedPicture.getAccount());
         assertEquals(caption, uploadedPicture.getCaption());
     }
@@ -75,13 +88,14 @@ public class PictureServiceTest
     {
         final String caption = "caption";
         final MockMultipartFile file = getTestFile("src/test/resources/invalidPicture.txt");
+        mockFileCreation();
         pictureService.uploadPicture(caption, file, account);
     }
 
     @Test
     public void testGetPictureDTOFromId()
     {
-        PictureDTO pic = pictureService.getPictureDTOFromId("1");
+        final PictureDTO pic = pictureService.getPictureDTOFromId("1");
         assertEquals("test account", pic.getAccount());
         assertEquals(1L, pic.getId());
     }
@@ -102,7 +116,7 @@ public class PictureServiceTest
     public void testGetAccountPictures()
     {
         Mockito.when(pictureRepository.findByAccount(account)).thenReturn(Collections.singletonList(pic));
-        List<Long> pictures = pictureService.getAccountPictures(account);
+        final List<Long> pictures = pictureService.getAccountPictures(account);
         assertEquals(1, pictures.size());
         assertEquals(1L, (long)pictures.get(0));
     }
@@ -113,7 +127,8 @@ public class PictureServiceTest
         assertEquals(picDTO, pictureService.toPictureDTO(pic));
     }
 
-    private MockMultipartFile getTestFile(String filePath) {
+    private MockMultipartFile getTestFile(String filePath)
+    {
         final File file = new File(filePath);
         MockMultipartFile mockMultipartFile = null;
         try {
@@ -122,5 +137,15 @@ public class PictureServiceTest
         } catch (final Exception e) {
         }
         return mockMultipartFile;
+    }
+
+    private void mockFileCreation()
+    {
+        try {
+            File tempFile = new File(tempFolder.getRoot().getPath() + "/pic.jpg");
+            PowerMockito.whenNew(File.class).withAnyArguments().thenReturn(tempFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
